af44b5f7b9a5b09b866cd075655aa244e3902a9f
TJ <tilwinjoy@gmail.com>
2016-04-17 22:08:17 +0530

    adding cached templates

--- dist/angular-bootstrap-material.js
+++ dist/angular-bootstrap-material.js
@@ -1,31 +1,29 @@
-angular.module('angularBootstrapMaterial', ['ngMessages']).
-controller('testCtrl', ['$scope', function ($scope) {
-    $scope.user = {}
-}]).run(function (abmConfig) {
-    //abmConfig.toggleErrorState();
-    abmConfig.setErrors({
-        minlength: "min length is something",
-        required: "required or something",
-        email: "something wrong with email"
-    });
-});
+angular.module('angularBootstrapMaterial', ['ngMessages']);
+angular.module("templates").run(["$templateCache", function ($templateCache) {
+    $templateCache.put("checkbox.html", "<label>\r\n    <abm-transclude-slot-checkbox></abm-transclude-slot-checkbox>\r\n    <span class=\"checkbox-material\">\r\n        <span class=\"check\"></span>\r\n    </span> {{label}}\r\n</label>");
+    $templateCache.put("form-group.html", "<div class=\"form-group\" ng-class=\"{{classList}}\" ng-form=\"fg_{{$id}}\">\r\n    <abm-transclude-slot></abm-transclude-slot>\r\n    <div ng-show=\"showErrors\" ng-messages=\"this[\'fg_\'+$id].$error\" ng-messages-multiple>\r\n        <div ng-if=\"errorMessagesInclude\" ng-messages-include=\"{{errorMessagesInclude}}\"></div>\r\n        <div ng-repeat=\"(key,value) in errorMessageMap\">\r\n            <!-- use ng-message-exp for a message whose key is given by an expression -->\r\n            <div ng-message-exp=\"key\" class=\"help-block\">{{value}}</div>\r\n        </div>\r\n    </div>\r\n</div>");
+    $templateCache.put("input.html", "<input id=\"input_{{$id}}\" name=\"input_{{$id}}\" class=\"form-control\">");
+    $templateCache.put("label.html", "<label for=\"{{\'input_\'+ formControl.$id}}\" class=\"control-label\">{{label}}</label>");
+    $templateCache.put("radio.html", "<label>\r\n    <abm-transclude-slot-radio></abm-transclude-slot-radio>\r\n    <span class=\"circle\"></span><span class=\"check\"></span> {{label}}\r\n</label>");
+    $templateCache.put("toggle.html", "<label>\r\n    {{label}}\r\n    <abm-transclude-slot-toggle></abm-transclude-slot-toggle>\r\n    <span class=\"toggle\"></span>\r\n</label>");
+}]);
 angular.module('angularBootstrapMaterial')
-    .directive('abmCheckbox', function(abmConfig) {
+    .directive('abmCheckbox', function (abmConfig) {
         return {
             scope: {
                 label: '@'
             },
             transclude: true,
             templateUrl: 'templates/checkbox.html',
-            link: function(scope, element, attrs, ctrl, transclude) {
-                transclude(function(clone, scope) {
+            link: function (scope, element, attrs, ctrl, transclude) {
+                transclude(function (clone, scope) {
                     element.find('abm-transclude-slot-checkbox').replaceWith(clone);
                 }, null);
             }
         }
     });
 angular.module('angularBootstrapMaterial')
-    .directive('abmLabel', function($compile) {
+    .directive('abmLabel', function ($compile) {
         return {
             /*templateUrl: 'templates/label.html',
     replace: true,*/
@@ -33,7 +31,7 @@ angular.module('angularBootstrapMaterial')
             scope: {
                 type: '='
             },
-            link: function(scope, element, attrs, formGroupCtrl) {
+            link: function (scope, element, attrs, formGroupCtrl) {
                 if (!formGroupCtrl) // if used outside a form group for some reason, do nothing
                     return false;
                 var className = 'label-' + (attrs.type || 'static'); // TBD see if this can be an array
@@ -44,14 +42,14 @@ angular.module('angularBootstrapMaterial')
         }
     })
 angular.module('angularBootstrapMaterial')
-    .directive('abmFormControl', ['isChar', function(isChar) {
+    .directive('abmFormControl', ['isChar', function (isChar) {
         return {
             /*templateUrl: 'templates/input.html',
     replace: true,*/
             scope: {},
             require: ["ngModel", '^abmFormGroup'],
-            compile: function() {
-                return function($scope, $element, attrs, ctrls) {
+            compile: function () {
+                return function ($scope, $element, attrs, ctrls) {
                     var input = ctrls[0];
                     var formGroup = ctrls[1];
                     if (!$element.hasClass('form-control')) // TBD checkif the condition is unnecessary
@@ -59,18 +57,18 @@ angular.module('angularBootstrapMaterial')
                     if (!$element.attr('id')) // TBD checkif the condition is unnecessary
                         $element.attr('id', 'form-control-' + $scope.$id);
                     formGroup.registerControl($scope);
-                    $element.on("input paste", function(e) {
+                    $element.on("input paste", function (e) {
                             if (isChar(e)) {
                                 formGroup.toggleEmpty(true);
                             }
-                        }).on("focus", function() {
+                        }).on("focus", function () {
                             formGroup.toggleFocus(true);
                         })
-                        .on("blur", function() {
+                        .on("blur", function () {
                             formGroup.toggleFocus(false);
                         });
 
-                    $scope.$watch(function() {
+                    $scope.$watch(function () {
                         return input.$invalid;
                     }, formGroup.toggleError);
                     $scope.$watch(attrs.ngModel, formGroup.toggleEmpty);
@@ -79,7 +77,7 @@ angular.module('angularBootstrapMaterial')
         }
     }]);
 angular.module('angularBootstrapMaterial')
-    .directive('abmFormGroup', function(abmConfig) {
+    .directive('abmFormGroup', function (abmConfig) {
         return {
             scope: {
                 classList: '=',
@@ -91,7 +89,7 @@ angular.module('angularBootstrapMaterial')
             replace: true,
             transclude: true,
             templateUrl: 'templates/form-group.html',
-            controller: function($scope, $element) {
+            controller: function ($scope, $element) {
                 $scope.errorMessageMap = {};
                 $scope.showErrors = abmConfig.getErrorState() && $scope.errorMessages !== false;
                 if ($scope.showErrors) {
@@ -101,28 +99,28 @@ angular.module('angularBootstrapMaterial')
                     else
                         angular.extend($scope.errorMessageMap, globalErrors);
                 }
-                this.toggleFocus = function(state) {
+                this.toggleFocus = function (state) {
                     $element.toggleClass("is-focused", state);
                 };
-                this.toggleEmpty = function(state) {
+                this.toggleEmpty = function (state) {
                     $element.toggleClass("is-empty", state);
                 }
-                this.toggleError = function(state) {
+                this.toggleError = function (state) {
                     $element.toggleClass("has-error", state);
                 }
-                this.registerControl = function(scope) {
+                this.registerControl = function (scope) {
                     $scope.formControl = scope;
                 }
-                this.addClass = function(className) {
+                this.addClass = function (className) {
                     $element.addClass(className);
                 }
 
             },
             link: {
-                pre: function($scope, element, attrs, ctrl, transclude) {
+                pre: function ($scope, element, attrs, ctrl, transclude) {
 
                 },
-                post: function(scope, element, attrs, ctrl, transclude) {
+                post: function (scope, element, attrs, ctrl, transclude) {
                     /*if (transclude.isSlotFilled('label')) {
                         var label = transclude(angular.noop, null, 'label');
                         element.find('label').replaceWith(label);
@@ -133,7 +131,7 @@ angular.module('angularBootstrapMaterial')
                         }, null, 'input');
                     }*/
 
-                    transclude(function(clone, scope) {
+                    transclude(function (clone, scope) {
                         var label = clone.find('label');
                         var formControl = clone.find('.form-control'); // TBD maybe defone an array of valid types
                         label.attr('for', 'form-control-' + scope.$id);
@@ -145,15 +143,15 @@ angular.module('angularBootstrapMaterial')
         }
     });
 angular.module('angularBootstrapMaterial')
-    .directive('abmRadio', function(abmConfig) {
+    .directive('abmRadio', function (abmConfig) {
         return {
             scope: {
                 label: '@'
             },
             transclude: true,
             templateUrl: 'templates/radio.html',
-            link: function(scope, element, attrs, ctrl, transclude) {
-                transclude(function(clone, scope) {
+            link: function (scope, element, attrs, ctrl, transclude) {
+                transclude(function (clone, scope) {
                     element.find('abm-transclude-slot-radio').replaceWith(clone);
                 }, null);
             }
