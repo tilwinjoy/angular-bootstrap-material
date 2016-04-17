57577905a075cc37fc64a06456e332dd9a4c5cda
tilwinjoy <tilwinjoy@gmail.com>
2016-04-17 18:58:07 +0530

    removing configuration from module definition

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,1 @@
-angular.module('angularBootstrapMaterial', ['ngMessages']).
-controller('testCtrl', ['$scope', function ($scope) {
-    $scope.user = {}
-}]).run(function (abmConfig) {
-    //abmConfig.toggleErrorState();
-    abmConfig.setErrors({
-        minlength: "min length is something",
-        required: "required or something",
-        email: "something wrong with email"
-    });
-});
+angular.module('angularBootstrapMaterial', ['ngMessages']);
