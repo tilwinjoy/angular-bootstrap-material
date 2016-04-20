8fba05f75566e3113e35a835711de8ea01c658b9
tilwinjoy <tilwinjoy@gmail.com>
2016-04-21 00:49:44 +0530

    adding ripple

--- scripts/directives/toggle.js
+++ scripts/directives/toggle.js
@@ -5,11 +5,20 @@ angular.module('angularBootstrapMaterial')
                 label: '@'
             },
             transclude: true,
+            require: '?^abmFormGroup',
             templateUrl: 'templates/toggle.html',
             link: function (scope, element, attrs, ctrl, transclude) {
                 transclude(function (clone, scope) {
                     element.find('abm-transclude-slot-toggle').replaceWith(clone);
                 }, null);
+
+                if (ctrl) {
+                    var $input = element.find('input');
+                    element.find('label').on('mouseenter mouseleave', function (event) {
+                        if ($input.prop('disabled')) return;
+                        ctrl.toggleFocus(event.type == 'mouseover');
+                    });
+                }
             }
         }
     });
