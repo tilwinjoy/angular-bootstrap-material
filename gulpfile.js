48d8dbf946c93bace5b3c261bce1b43b9cb32d12
tilwinjoy <tilwinjoy@gmail.com>
2016-05-10 11:03:21 +0530

    updating gulpfile to remove temporary templatechache file and improve beautifier config

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,40 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename'),
-    templateCache = require('gulp-angular-templatecache');;
+  concat = require('gulp-concat'),
+  prettify = require('gulp-jsbeautifier'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename'),
+  templateCache = require('gulp-angular-templatecache');
+var merge2 = require('merge2');
 
-gulp.task('cache-templates', function () {
-    return gulp.src('templates/**/*.html')
-        .pipe(templateCache({
-            module: 'angularBootstrapMaterial',
-            transformUrl: function (url) {
-                return 'templates/' + url;
-            }
-        }))
-        .pipe(gulp.dest('scripts/'));
+// beautify task
+gulp.task('beautify', function () {
+  return gulp.src(['src/scripts/**/*.js',
+                   'src/templates/**/*.html',
+                   'demos/scripts/**/*.js',
+                  'demos/scripts/**/*.css',
+                   'demos/scripts/**/*.css'], {
+      base: './'
+    })
+    .pipe(prettify({
+      debug: true
+    }))
+    .pipe(gulp.dest('./'));
 });
-gulp.task('default', ['cache-templates'], function () {
-    return gulp.src('scripts/**/*.js')
-        .pipe(concat('angular-bootstrap-material.js'))
-        .pipe(gulp.dest('dist/'))
-        .pipe(rename('angular-bootstrap-material.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/'));
+
+
+//Below task combines the templateCache stream and source files and outputs concatinated file
+gulp.task('default', ['beautify'], function () {
+  return merge2(gulp.src('src/scripts/**/*.js'),
+      gulp.src('src/templates/**/*.html')
+      .pipe(templateCache({
+        module: 'angularBootstrapMaterial',
+        transformUrl: function (url) {
+          return 'templates/' + url;
+        }
+      })))
+    .pipe(concat('angular-bootstrap-material.js'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(uglify())
+    .pipe(rename('angular-bootstrap-material.min.js'))
+    .pipe(gulp.dest('dist/'));
 });
