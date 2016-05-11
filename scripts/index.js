a409c8c77d417592dcf22bcc93931b7c4631ef5c
tilwinjoy <tilwinjoy@gmail.com>
2016-05-11 16:05:27 +0530

    adding docs for bootstrap components

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-angular.module('angularBootstrapMaterialDocs', ['angularBootstrapMaterial', 'ui.bootstrap']);
+angular.module('angularBootstrapMaterialDocs', ['angularBootstrapMaterial', 'ui.bootstrap'])
+  .config(function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+    // Allow same origin resource loads.
+    'self',
+    // Allow loading from our assets domain.  Notice the difference between * and **.
+    'https://rawgit.com/**'
+  ]);
+  });
