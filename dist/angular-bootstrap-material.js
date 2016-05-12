c0537d229c6f2297c03005a1a280b6e07bb113cc
TJ <tilwinjoy@gmail.com>
2016-05-12 23:46:34 +0530

    updating dist

--- dist/angular-bootstrap-material.js
+++ dist/angular-bootstrap-material.js
@@ -7,18 +7,26 @@ angular.module('angularBootstrapMaterial')
         label: '@'
       },
       transclude: true,
-      require: '?^abmFormGroup',
+      require: ['?^abmFormGroup'],
       templateUrl: 'templates/checkbox.html',
-      link: function (scope, element, attrs, ctrl, transclude) {
-        transclude(function (clone, scope) {
-          element.find('abm-transclude-slot-checkbox').replaceWith(clone);
+      link: function ($scope, $element, attrs, ctrls, transclude) {
+        var input;
+        var formGroup = ctrls[0];
+        transclude(function (clone, $scope) {
+          $element.find('abm-transclude-slot-checkbox').replaceWith(clone);
         }, null);
-        if (ctrl) {
-          var $input = element.find('input');
-          element.find('label').on('mouseenter mouseleave', function (event) {
+        if (formGroup) {
+          var $input = $element.find('input');
+          input = $input.controller('ngModel');
+          $element.find('label').on('mouseenter mouseleave', function (event) {
             if ($input.prop('disabled')) return;
-            ctrl.toggleFocus(event.type == 'mouseover');
+            formGroup.toggleFocus(event.type == 'mouseover');
           });
+          if (input) {
+            $scope.$watch(function () {
+              return input.$invalid;
+            }, formGroup.toggleError);
+          }
         }
       }
     }
@@ -91,7 +99,7 @@ angular.module('angularBootstrapMaterial')
       /*templateUrl: 'templates/input.html',
     replace: true,*/
       scope: {},
-      require: ["ngModel", '^abmFormGroup'],
+      require: ['ngModel', '^abmFormGroup'],
       compile: function () {
         return function ($scope, $element, attrs, ctrls) {
           var input = ctrls[0];
@@ -101,15 +109,17 @@ angular.module('angularBootstrapMaterial')
           if (!$element.attr('id')) // TBD checkif the condition is unnecessary
             $element.attr('id', 'form-control-' + $scope.$id);
           formGroup.registerControl($scope);
-          $element.on("input paste", function (e) {
+          $element.on('input paste', function (e) {
               if (isChar(e)) {
                 formGroup.toggleEmpty(true);
               }
-            }).on("focus", function () {
+            }).on('focus', function () {
               formGroup.toggleFocus(true);
             })
-            .on("blur", function () {
+            .on('blur', function () {
               formGroup.toggleFocus(false);
+              if ($element.val())
+                formGroup.toggleEmpty(false);
             });
 
           $scope.$watch(function () {
@@ -197,17 +207,23 @@ angular.module('angularBootstrapMaterial')
       transclude: true,
       require: '?^abmFormGroup',
       templateUrl: 'templates/radio.html',
-      link: function (scope, element, attrs, ctrl, transclude) {
-        transclude(function (clone, scope) {
+      link: function ($scope, element, attrs, formGroup, transclude) {
+        transclude(function (clone, $scope) {
           element.find('abm-transclude-slot-radio').replaceWith(clone);
         }, null);
 
-        if (ctrl) {
+        if (formGroup) {
           var $input = element.find('input');
+          input = $input.controller('ngModel');
           element.find('label').on('mouseenter mouseleave', function (event) {
             if ($input.prop('disabled')) return;
-            ctrl.toggleFocus(event.type == 'mouseover');
+            formGroup.toggleFocus(event.type == 'mouseover');
           });
+          if (input) {
+            $scope.$watch(function () {
+              return input.$invalid;
+            }, formGroup.toggleError);
+          }
         }
       }
     }
