2a14d890c63bbc2dd05dfff2aac76979ef81325b
tilwinjoy <tilwinjoy@gmail.com>
2016-06-04 13:23:06 +0530

    Fixing navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
-angular.module('angularBootstrapMaterialDocs', ['angularBootstrapMaterial', 'ui.bootstrap'])
+angular.module('angularBootstrapMaterialDocs', ['angularBootstrapMaterial', 'ngAnimate', 'ui.bootstrap'])
   .config(function ($sceDelegateProvider) {
     $sceDelegateProvider.resourceUrlWhitelist([
     // Allow same origin resource loads.
     'self',
     // Allow loading from our assets domain.  Notice the difference between * and **.
     'https://rawgit.com/**'
-  ]);
+    ]);
+  })
+  .controller('navController', function ($scope) {
+    $scope.isCollapsed = true;
   });
